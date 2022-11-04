4920d7f491d1d49962f3c5f794f8268ad5e19691
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-04 16:37:46 +0700

    Menambahkan variabel

--- Exercise2_20210140064/Program.cs
+++ Exercise2_20210140064/Program.cs
@@ -4,5 +4,12 @@ namespace Exercise2
 {
     class Program
     {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array 
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+        
     }
 }
